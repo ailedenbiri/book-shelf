d99d0f2ad031edbceb11d37f4dc27516efe80f76
Muhammed Kaba <muhammed.kaba@outlook.com>
2024-02-15 11:19:50 +0300

    shelfinfos

--- Assets/DEV/Scripts/GameManager.cs
+++ Assets/DEV/Scripts/GameManager.cs
@@ -64,7 +64,7 @@ public class GameManager : MonoBehaviour
     public void FadeShelfInfos()
     {
         Sequence seq = DOTween.Sequence();
-        CanvasGroup c = GameObject.Find("ShelfInfoCanvas").GetComponent<CanvasGroup>();
+        CanvasGroup c = GameObject.Find("ShelfInfoCanvas").transform.GetChild(0).GetComponent<CanvasGroup>();
         c.DOKill();
         seq.SetId(c);
         seq.Append(c.DOFade(1f, 0.6f));
